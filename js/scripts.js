be34d7530e9231e9c3d857d06c117f053c491132
Jonathan Thom <jonathan.thom1990@gmail.com>
2016-10-28 08:49:46 -0700

    Add function that will change every 5th number (except those divisible by 15) to pong

--- js/scripts.js
+++ js/scripts.js
@@ -8,15 +8,17 @@ function isValid(userInput) {
 function pingPong(userInput) {
   var outputNumbers = [];
 
-  for (var i = 0; i <= userInput; i++) {
+  for (var i = 1; i <= userInput; i++) {
     outputNumbers.push(i);
   }
 
   outputNumbers.forEach(function(number) {
-    if (number % 15 === 0 && number != 0) {
-      console.log("if statement is running");
+    if (number % 15 === 0) {
       var fifteen = outputNumbers.indexOf(number);
       outputNumbers[fifteen] = "pingpong";
+    } else if (number % 5 === 0) {
+      var five = outputNumbers.indexOf(number);
+      outputNumbers[five] = "pong";
     }
   });
 
